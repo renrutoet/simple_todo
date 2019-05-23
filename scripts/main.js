209ffc0cde9fee041b6559614ca77565e8e2cfcf
unknown <teoturner@gmail.com>
2019-05-23 18:08:38 +0100

    working edit and delete functionallity, with a cancel and confirm button when editing

--- scripts/main.js
+++ scripts/main.js
@@ -7,20 +7,93 @@ const list = document.querySelector("#todoList");
 
 inputBtn.addEventListener("click",function(){
 
-    let task = document.createElement("li");
+    //task input box
+    var task = document.createElement("li");
+    var inputValue = input.value;
+    task.innerHTML = inputValue;
+
+    //delete button
     let deleteBtn = document.createElement("span");
-    deleteBtn.innerHTML = "delete";
+    deleteBtn.innerHTML = "Delete";
     deleteBtn.classList.add("delete");
 
     deleteBtn.addEventListener("click",function(){
         this.parentNode.parentNode.removeChild(this.parentNode);
     });
+
+    //edit button
+    //create edit button
+    let editBtn = document.createElement("span");
+    editBtn.innerHTML = "Edit"
+    editBtn.classList.add("edit");
+
+    //on click remove task on list and replace with input box for editing
+    editBtn.addEventListener("click",function(){
+
+        //stores the task
+        let oldTaskText = inputValue;
+        
+        //removes list item to be replaced by the input element
+        this.parentNode.parentNode.removeChild(this.parentNode);
+
+        //create input element for editing the value
+        let newTaskInput = document.createElement("input");
+        newTaskInput.placeholder = "Enter new Task";
+        newTaskInput.type="text";
+        newTaskInput.value = oldTaskText;
+
+        //creating the cancel button
+        let cancelBtn = document.createElement("span");
+        cancelBtn.innerHTML = "Cancel";
+        cancelBtn.classList.add("delete");
+
+        //on click remove buttons an input box & replace with unedited list element
+        cancelBtn.addEventListener("click",function(){
+            //remove buttons
+            this.parentNode.removeChild(this.previousSibling);
+            this.parentNode.removeChild(this.nextSibling);
+            this.parentNode.removeChild(this);
+
+            //recreate old list item
+            let oldTask = document.createElement("li");
+            oldTask.innerHTML = oldTaskText;
+
+            //add back into the DOM
+            oldTask.appendChild(editBtn);
+            oldTask.appendChild(deleteBtn);
+            list.appendChild(oldTask);
+        });
+
+        let confirmBtn = document.createElement("span");
+        confirmBtn.innerHTML = "Confirm";
+        confirmBtn.classList.add("edit");
+
+        confirmBtn.addEventListener("click",function(){
+            //remove buttons
+            this.parentNode.removeChild(this.previousSibling.previousSibling);
+            this.parentNode.removeChild(this.previousSibling);
+            this.parentNode.removeChild(this);
+
+            //store new value as new task
+            let newTask = document.createElement("li");
+            newTask.innerHTML = newTaskInput.value;
+            inputValue = newTaskInput.value;
+
+            //add back into the DOM
+            newTask.appendChild(editBtn);
+            newTask.appendChild(deleteBtn);
+            list.appendChild(newTask);
+        });
+
+        //replace text with input
+        list.appendChild(newTaskInput);
+        list.appendChild(cancelBtn);
+        list.appendChild(confirmBtn);
+    });
     
-    task.innerHTML = input.value;
+    //DOM interaction
+    task.appendChild(editBtn);
     task.appendChild(deleteBtn);
-
-    
-
     list.appendChild(task);
 
 });
